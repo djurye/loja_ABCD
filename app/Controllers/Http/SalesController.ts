99594f18aae0769dc6418899ebac6d06686cb88e
IurydJR <iury.djr.ti@gmail.com>
2024-09-30 20:30:54 -0300

    refactor(sale controller): add response status to controller class and integrates with pivot table sales_products and products

--- app/Controllers/Http/SalesController.ts
+++ app/Controllers/Http/SalesController.ts
@@ -1,53 +1,85 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import Sale from 'App/Models/Sale'
+import Database from '@ioc:Adonis/Lucid/Database'
 
 export default class SalesController {
-  public async index({}: HttpContextContract) {
-        
-    return await Sale.all()
+  
+  public async index({ response }: HttpContextContract) {
+    try {
+      const sales = await Sale.query().preload('products')
+      return response.status(200).json(sales) 
+    } catch (error) {
+      return response.status(500).json({ message: 'Erro ao buscar vendas', error })
+    }
   }
 
   public async create({}: HttpContextContract) {}
 
-  public async store({request}: HttpContextContract) {
-    const sale:Sale = await Sale.create(request.all())
-    return sale
-  }
-
-  public async show({params}: HttpContextContract) {
-    const sale = await Sale.find(params.id)
+  public async store({ request,response }: HttpContextContract) {
+    try {
+      const { id_product, ...data } = request.only(['date', 'total', 'id_product', 'amount'])
+      const sale = await Sale.create(data)
     
-    if (!sale) {
-      return { message: 'Sale not found' }
+      if (id_product && id_product.length > 0) {
+        await sale.related('products').attach(id_product)
+        await sale.load('products')
+      }
+
+      return response.status(201).json(sale)
+    } catch (error) {
+      return response.status(500).json({ message: 'Erro ao adicionar venda', error })
     }
-
-    return sale
-
   }
 
-  public async edit({}: HttpContextContract) {}
+  public async show({params, response}: HttpContextContract) {
+    try {
+      const sale = await Sale.query().where('id', params.id).preload('products').first()
 
-  public async update({request}: HttpContextContract) {
-    const sale:Sale|null = await Sale.find(request.param('id'))
+      if (!sale) {
+        return response.status(404).json({ message: 'Venda não encontrada' })
+      }
 
-    if (!sale) {
-      return { message: 'sale not found' }
+      return response.status(200).json(sale)
+    } catch (error) {
+      return response.status(500).json({ message: 'Erro ao buscar venda', error })
     }
-
-    sale.date = request.input('date')
-    sale.total = request.input('total')
-    await sale?.save()
-
-    return sale
   }
 
-  public async destroy({request}: HttpContextContract) {
-    const sale:Sale|null = await Sale.find(request.param('id'))
-
-    if (!sale) {
-      return { message: 'sale not found' }
-    }
+  public async update({ params, request, response }: HttpContextContract) {
+    try {
+      const sale = await Sale.findOrFail(params.id)
+      const { id_product, ...data } = request.only(['date', 'total', 'id_product', 'amount'])
     
-    sale.delete()
+      if (!sale) {
+        return response.status(404).json({ message: 'Venda não encontrada' })
+      }
+
+      sale.merge(data)
+      await sale.save()
+    
+      if (id_product && id_product.length > 0) {
+        await sale.related('products').sync(id_product)
+      }
+    
+      await sale.load('products') 
+      return response.status(200).json(sale)
+    } catch (error) {
+      return response.status(500).json({ message: 'Erro ao atualizar produto', error })
+    }
+  }
+
+  public async destroy({request,response}: HttpContextContract) {
+    try {
+      const sale:Sale|null = await Sale.find(request.param('id'))
+
+      if (!sale) {
+        return response.status(404).json({ message: 'Venda não encontrada' })
+      }
+      
+      sale.delete()
+      return response.status(200).json({ message: 'Produto deletado com sucesso' })
+    } catch (error) {
+      return response.status(500).json({ message: 'Erro ao deletar produto', error })
+    }
   }
 }
